c29844957fde575608c28d935540f3b07001f4be
Lasse Krarup <au579525@ase.au.dk>
2018-09-10 22:47:47 +0200

    Added four circles with icons and stuff

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-    setTimeout(function() {
-        $("#loadwrap").fadeOut(1500);
-    }, 2000);
+$(document).ready(function() {
+    var w = $(".circle").css("width");
+    $(".circle").css("height", w);
+});
+
+$(window).on('load', function() {
+    $("#loadwrap").fadeOut(1500);
+});
+
+$(window).resize(function() {
+    $(".circle").width("10%");
+    var w = $(".circle").width();
+    $(".circle").height(w);
 });
