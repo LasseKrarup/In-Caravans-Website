e37196a7238e45006cd3178ce56e7ad5faad51ad
Lasse Krarup <au579525@ase.au.dk>
2019-02-19 07:47:53 +0100

    Added .gitignore. Added gulp, autoprefixer and gulp-sass

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+function defaultTask(cb) {
+	// place code for your default task here
+	console.log("Building...");
+
+	cb();
+}
+
+function build(cb){
+    
+    
+    cb();
+}
+
+exports.default = defaultTask
